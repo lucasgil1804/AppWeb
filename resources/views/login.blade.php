b5c9768980935d09fbb31927df5c50b6ddec7f5b
FernandezJuan017 <juancruzf017@gmail.com>
2019-03-11 12:31:29 -0300

    Estilo login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Login V15</title>
+    <title>Quick Fix - Login</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->  
@@ -34,20 +34,20 @@
             <div class="wrap-login100">
                 <div class="login100-form-title" style="background-image: url(../img/bg-01.jpg);">
                     <span class="login100-form-title-1">
-                        Sign In
+                       Quick Fix - Iniciar Sesión
                     </span>
                 </div>
 
                 <form class="login100-form validate-form">
                     <div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
-                        <span class="label-input100">Username</span>
-                        <input class="input100" type="text" name="username" placeholder="Enter username">
+                        <span class="label-input100">Usuario:</span>
+                        <input class="input100" type="text" name="username" placeholder="Ingrese su email">
                         <span class="focus-input100"></span>
                     </div>
 
                     <div class="wrap-input100 validate-input m-b-18" data-validate = "Password is required">
-                        <span class="label-input100">Password</span>
-                        <input class="input100" type="password" name="pass" placeholder="Enter password">
+                        <span class="label-input100">Contraseña:</span>
+                        <input class="input100" type="password" name="pass" placeholder="Ingrese su contraseña">
                         <span class="focus-input100"></span>
                     </div>
 
@@ -55,13 +55,13 @@
                         <div class="contact100-form-checkbox">
                             <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
                             <label class="label-checkbox100" for="ckb1">
-                                Remember me
+                                Recordarme
                             </label>
                         </div>
 
                         <div>
                             <a href="#" class="txt1">
-                                Forgot Password?
+                                Olvidaste tu contraseña?
                             </a>
                         </div>
                     </div>
