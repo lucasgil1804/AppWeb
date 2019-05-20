734e71284c4b4c1c729d272ddba529786f60dc14
lucasgil1804 <lucasgil.1804@gmail.com>
2019-05-20 11:49:04 -0300

    Agrego vista verDetalle

--- resources/views/Admin/verDetalle.blade.php
+++ resources/views/Admin/verDetalle.blade.php
@@ -1,8 +1,28 @@
+
 @extends('layouts.estiloAdmin')
 
 
 @section('contenidoAdmin')
+<div class="card" style="margin-right: 20px; margin-left: 20px;" >
+	<div class="card-header"><h3>Detalle de Usuario</h3></div>
+		<div class="card-body card-block">
 
+			<p class="mb-3"><b>DNI:</b> {{ $user->dni }}</p>
+			<p class="mb-3"><b>Nombre y Apellido:</b> {{ $user->nombre. " " .$user->apellido }}</p>
+			<p class="mb-3"><b>Email:</b> {{ $user->email }}</p>
+			@if ( $user->estado == 1)
+				<p class="mb-3"><b>Estado:</b> Activo</p>
+			@else
+				<p class="mb-3"><b>Estado:</b> Inactivo</p>
+			@endif
+			<button class="btn btn-primary" type="submit" title="Regresar">
+				<a href="{{ url()->previous() }}"></a>
+				<i class="fa fa-arrow-circle-left"></i>
+				Regresar
+			</button>
+			
+		</div>
+</div>
 
 
 @endsection
