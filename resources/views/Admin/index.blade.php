5ef65267bc4a10c0a091cbca72d012b482ec193d
lucasgil1804 <lucasgil.1804@gmail.com>
2019-06-06 19:10:02 -0300

    Modifico vista index

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -8,94 +8,59 @@
 @section('contenidoAdmin')
 
 <!-- USER DATA-->
-                                <div class="user-data m-b-30 m-r-30 m-l-30">
-                                    <h3 class="title-3 m-b-30">
-                                        <i class="zmdi zmdi-account-calendar"></i>Datos de Usuario</h3>
-                                    <div class="table table-data">
-                                        <table class="table">
-                                            <thead>
-                                                <tr>
-                                                    <td>Nombre</td>
-                                                    <td>Age</td>
-                                                    <td>role</td>
-                                                    <td>Salary</td>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
+<div class="user-data m-b-30 m-r-30 m-l-30">
+    <h3 class="title-3 m-b-30">
+        <i class="zmdi zmdi-account-calendar"></i>Datos de Usuarios</h3>
+            <div class="table table-data">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <td>Nombre</td>
+                            <td>DNI</td>
+                            <td>Rol</td>
+                            <td>Estado</td>
+                        </tr>
+                    </thead>
+                <tbody>
+
+                    @foreach ($users as $user)
+                        <tr>
                                                     
-                                                    <td>
-                                                        <div class="table-data__info">
-                                                            <h6>lori lynch</h6>
-                                                            <span>
-                                                                <a href="#">johndoe@gmail.com</a>
-                                                            </span>
-                                                        </div>
-                                                    </td>
-                                                    <td>32</td>
-                                                    <td>
-                                                        <span class="role admin">admin</span>
-                                                    </td>
-                                                    <td>$320,800</td>
-                                                    
-                                                </tr>
-                                                <tr>
-                                                    
-                                                    <td>
-                                                        <div class="table-data__info">
-                                                            <h6>lori lynch</h6>
-                                                            <span>
-                                                                <a href="#">johndoe@gmail.com</a>
-                                                            </span>
-                                                        </div>
-                                                    </td>
-                                                    <td>27</td>
-                                                    <td>
-                                                        <span class="role user">Empleado</span>
-                                                    </td>
-                                                    <td>$170,750</td>
-                                                    
-                                                </tr>
-                                                <tr>
-                                                    
-                                                    <td>
-                                                        <div class="table-data__info">
-                                                            <h6>lori lynch</h6>
-                                                            <span>
-                                                                <a href="#">johndoe@gmail.com</a>
-                                                            </span>
-                                                        </div>
-                                                    </td>
-                                                    <td>41</td>
-                                                    <td>
-                                                        <span class="role user">Empleado</span>
-                                                    </td>
-                                                    <td>$86,000</td>
-                                                    
-                                                </tr>
-                                                <tr>
-                                                    
-                                                    <td>
-                                                        <div class="table-data__info">
-                                                            <h6>lori lynch</h6>
-                                                            <span>
-                                                                <a href="#">johndoe@gmail.com</a>
-                                                            </span>
-                                                        </div>
-                                                    </td>
-                                                    <td>22</td>
-                                                    <td>
-                                                        <span class="role member">Técnico</span>
-                                                    </td>
-                                                    <td>$433,060</td>
-                                                    
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                    
+                            <td>
+                                <div class="table-data__info">
+                                    <h6>{{ $user->nombre. " " .$user->apellido }}</h6>
+                                        <span>
+                                            {{ $user->email }}
+                                        </span>
                                 </div>
-                                <!-- END USER DATA-->
+                            </td>
+                            <td>{{ $user->dni }}</td>
+                            <td>
+                                @if( $user->id_tipoUsuario == 1 )
+                                    <span class="role admin">{{ $user->tipoUsuario->descripcion }}</span>
+                                @elseif ( $user->id_tipoUsuario == 2 )
+                                    <span class="role employ">{{ $user->tipoUsuario->descripcion }}</span>
+                                @elseif ( $user->id_tipoUsuario == 3 )
+                                    <span class="role tech">{{ $user->tipoUsuario->descripcion }}</span>
+                                @else
+                                    <span class="role client">{{ $user->tipoUsuario->descripcion }}</span>
+                                @endif
+                            </td>
+                            @if ( $user->deleted_at == null)
+                                <td><i style="color: green;" class="fa fa-arrow-circle-up" data-toggle="tooltip" data-placement="top" title="Activo"></i></td>
+                            @else
+                                <td><i style="color: red;" class="fa fa-arrow-circle-down" data-toggle="tooltip" data-placement="top" title="Inactivo"></i></td>
+                            @endif
+                                                    
+                        </tr>
+                    @endforeach
+                                                
+                </tbody>
+                </table>
+            </div>
+                                    
+</div>
+<!-- END USER DATA-->
 
 @endsection
 
