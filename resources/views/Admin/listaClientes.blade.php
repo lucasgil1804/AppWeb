11902bf1811eee377b457edf43371b4843e9edc0
FernandezJuan017 <juancruzf017@gmail.com>
2019-06-12 17:47:38 -0300

    Agrego vista listaClientes

--- resources/views/Admin/listaClientes.blade.php
+++ resources/views/Admin/listaClientes.blade.php
@@ -53,17 +53,19 @@
                 <th>Nombre</th>
                 <th>Apellido</th>
                 <th>Email</th>
+                <th>Tipo Usuario</th>
                 <th>Estado</th>
                 <th>Acción</th>
             </tr>
         </thead>
         <tbody>
-            @foreach ($listaEmpleados as $user)
+            @foreach ($listaClientes as $user)
             <tr>
                 <td>{{ $user->dni }}</td>
                 <td>{{ $user->nombre }}</td>
                 <td>{{ $user->apellido }}</td>
                 <td>{{ $user->email }}</td>
+                <td>{{ $user->tipoUsuario->descripcion }}</td>
                 @if ( $user->deleted_at == null)
                     <td><i style="color: green;" class="fa fa-arrow-circle-up" data-toggle="tooltip" data-placement="top" title="Activo"></i></td>
                 @else
