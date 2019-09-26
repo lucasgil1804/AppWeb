338804aad1e488bd9a388b3723693f40fce0a47d
FernandezJuan017 <juancruzf017@gmail.com>
2019-09-26 10:45:26 -0300

    Cambio en la tabla usuario (password nulo)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
             $table->integer('dni');
             $table->string('email',40)->unique();
             //$table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             //$table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
