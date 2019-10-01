ed049069854cb563fa7ad78028ded0e2845ae215
FernandezJuan017 <juancruzf017@gmail.com>
2019-10-01 12:11:10 -0300

    Cambios en el estilo

---
+++ database/migrations/2019_10_01_115050_marcas.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Marcas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marcas', function (Blueprint $table) {
+            $table->increments('id_marca');
+            $table->string('descripcion',30)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marcas');
+    }
+}
---
+++ database/migrations/2019_10_01_115930_equipos.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Equipos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipos', function (Blueprint $table) {
+            $table->increments('id_equipos');
+            $table->unsignedInteger('id_marca');
+            $table->foreign('id_marca')->references('id_marca')->on('marcas');
+            $table->unsignedInteger('id_tipoEquipo');
+            $table->foreign('id_tipoEquipo')->references('id_tipoEquipo')->on('tipoequipos');
+            $table->string('modelo',30);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipos');
+    }
+}
