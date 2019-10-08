d8a4831f52303a5601dfc33f851b953775977afc
lucasgil1804 <lucasgil.1804@gmail.com>
2019-10-08 18:44:54 -0300

    Agrego Modelos Estado, Reparacion y Detalle

---
+++ app/Models/Reparacion.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Reparacion extends Model
+{
+	use SoftDeletes;
+
+    public $table = 'reparaciones';
+
+    protected $primaryKey = 'id_reparacion';
+
+    protected $fillable = [
+    	'id_usuario','id_equipo','fecha_ingreso','fecha_egreso','plazo_estimado','total',
+    ];
+
+    public function usuario()
+    {
+    	return $this->belongsTo(User::class, 'id_usuario');
+    }
+
+    public function equipo()
+    {
+    	return $this->belongsTo(equipos::class, 'id_equipo');
+    }
+}
