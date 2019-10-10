6e1d27ced88c5fcdf9499b808d06e271e759ece0
lucasgil1804 <lucasgil.1804@gmail.com>
2019-10-10 12:54:24 -0300

    Cambio estilo formulario Estado Equipo

--- resources/views/estadoEquipo.blade.php
+++ resources/views/estadoEquipo.blade.php
@@ -21,17 +21,29 @@
 
 			<div class="container" align="center">
 				<div class="section-top-border">
-							<div class="col-lg-8 col-md-8">
+							<div class="col-lg-9 col-md-9">
 								<h1>¿Ya está listo mi equipo?</h1>
 								<h3 class="mb-30 mt-20" style="color: grey;">Ingresá tu código</h3>
 								<form action="#">
-									<div class="input-group-icon mt-10">
-										<div class="icon"><i class="fa fa-hashtag" aria-hidden="true" style="margin-top: 12px;"></i></div>
-										<input type="text" name="seguimiento" placeholder="N° de seguimiento" onfocus="this.placeholder = ''" onblur="this.placeholder = 'N° de seguimiento'" required class="single-input">
+									<!-- <div class="input-group-icon mt-10"> -->
+										<!-- <div class="icon"><i class="fa fa-hashtag" aria-hidden="true" style="margin-top: 12px;"></i></div> -->
+										
+										<div class="input-group" align="center">
+											<div class="input-group-prepend">
+											<span class="input-group-text">#</span>
+											</div>
+											<input type="text" name="seguimiento" placeholder="N° de seguimiento" onfocus="this.placeholder = ''" onblur="this.placeholder = 'N° de seguimiento'" required class="form-control" style="line-height:40px;border:none;outline:none;background:#f9f9ff;padding:0 20px;">
+
+										<!-- <div class="icon"><i class="fa fa-minus" aria-hidden="true" style="margin-top: 12px;"></i></div> -->
+											<div class="input-group-prepend">
+												<span class="input-group-text">/</span>
+											</div>
+											<input type="text" name="seguimiento2" required class="form-control" style="line-height:40px;border:none;outline:none;background:#f9f9ff;padding:0 20px;">
+										</div>
+
 										<div class="button-group-area mt-40">
 											<a href="#" class="genric-btn danger circle arrow">Consultar<span class="lnr lnr-arrow-right"></span></a>
 										</div>
-									</div>
 								</form>
 							</div>
 					</div>
